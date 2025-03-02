5322c78251572bc80fc0f34df18d4b86c016c80a
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-03-02 17:35:08 +1100

    Added calculator functions, no UI interaction yet

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let equation = {
-    num1: 0,
+    num1: "0",
     operator: null,
-    num2: null
+    num2: "0"
 }
+let currentDisplayNumber = equation.num1;
+let newCalcuation = true;
 
 function add(num1, num2){
     return num1 + num2;
@@ -21,13 +23,15 @@ function divide(num1, num2){
 }
 
 function operate(num1, operator, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     let answer;
     switch (operator){
         case "+":
             answer = add(num1, num2);
             break;
         case "-":
-            answer = ubtract(num1, num2);
+            answer = subtract(num1, num2);
             break;
         case "*":
             answer = multiply(num1, num2);
@@ -39,6 +43,11 @@ function operate(num1, operator, num2){
             answer = null;
             alert("ERROR: invalid operator");
     }
+    equation.num1 = answer;
+    equation.operator = null;
+    equation.num2 = "0";
+    newCalcuation = true;
+
     return answer;
 }
 
@@ -50,4 +59,30 @@ function percent(num){
 }
 
 function operation(input){
+    // input is not number, isNaN() = is Not a Number
+    if (isNaN(input)){ 
+        equation.operator = input;
+    }
+
+    // input is number
+    else{ 
+        // first number
+        if (!equation.operator){
+            if (equation.num1 == "0" || newCalcuation)
+                equation.num1 = input;
+            else
+                equation.num1 = equation.num1 + input;
+            newCalcuation = false;
+            currentDisplayNumber = equation.num1;
+        }
+        // second number
+        else{
+            if (equation.num2 == "0")
+                equation.num2 = input;
+            else
+                equation.num2 = equation.num2 + input;
+            currentDisplayNumber = equation.num2;
+        }
+
+    } 
 }
